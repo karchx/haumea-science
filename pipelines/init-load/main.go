82dae97432decd90e381d75872bd21c33570efe4
karchx <kenethra.74@gmail.com>
2026-05-02 08:39:00 -0600

    feat: add parquet to parquet

--- pipelines/init-load/main.go
+++ pipelines/init-load/main.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/apache/arrow/go/v17/parquet"
 	"github.com/apache/arrow/go/v17/parquet/compress"
+	"github.com/apache/arrow/go/v17/parquet/file"
 	"github.com/apache/arrow/go/v17/parquet/pqarrow"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -68,10 +69,6 @@ func main() {
 	ctx := context.Background()
 
 	for _, tbl := range tablesConfig {
-		if strings.ToLower(tbl.Format) != "csv" {
-			continue
-		}
-
 		for i := range tbl.Partitions {
 			part := &tbl.Partitions[i]
 			if part.PartitionKey == "year" && part.PartitionValue == "current_year" {
@@ -84,8 +81,19 @@ func main() {
 				part.PartitionValue = time.Now().Format("02")
 			}
 		}
-		log.Infof("Processing table: %s", tbl.TableName)
-		processCSVToParquet(ctx, tbl, minioClient, pool)
+
+		if strings.ToLower(tbl.Format) == "csv" {
+			log.Infof("Processing table: %s format: %s", tbl.TableName, tbl.Format)
+			processCSVToParquet(ctx, tbl, minioClient, pool)
+		} else if strings.ToLower(tbl.Format) == "parquet" {
+			log.Infof("Processing table: %s format: %s", tbl.TableName, tbl.Format)
+			processParquetToParquet(ctx, tbl, minioClient, pool)
+		} else {
+			log.Infof("Table format %s not support", tbl.Format)
+			return
+		}
+
+		log.Infof("Process complete")
 	}
 }
 
@@ -125,11 +133,7 @@ func processCSVToParquet(ctx context.Context, config TablesConfig, minioClient *
 	dataLen := int64(buf.Len())
 
 	// generate partions
-	partitionPath := fmt.Sprintf("%s/%s/", config.Dest, config.TableName)
-	for _, part := range config.Partitions {
-		partitionPath += fmt.Sprintf("%s=%s/", part.PartitionKey, part.PartitionValue)
-	}
-	partitionPath += fmt.Sprintf("part-%s.parquet", uuid.New().String())
+	partitionPath := createPathPartition(config)
 
 	info, err := minioClient.PutObject(ctx, bucketName, partitionPath, bytes.NewReader(finalData), dataLen, minio.PutObjectOptions{
 		ContentType: "application/octet-stream",
@@ -142,45 +146,126 @@ func processCSVToParquet(ctx context.Context, config TablesConfig, minioClient *
 	log.Infof("Successfully uploaded Parquet file to MinIO: %s (size: %d bytes)", info.Key, info.Size)
 }
 
-func readFileRemote(ctx context.Context, minioClient *minio.Client, objectName string, pool memory.Allocator) (arrow.Table, error) {
+func processParquetToParquet(ctx context.Context, config TablesConfig, minioClient *minio.Client, pool memory.Allocator) {
+	table, err := readFileRemote(ctx, minioClient, config.Source, config.Format, pool)
+	if err != nil {
+		log.Errorf("Failed to read source file from MinIO: %v", err)
+		return
+	}
+	defer table.Release()
+
+	var buf bytes.Buffer
+
+	writerProps := parquet.NewWriterProperties(
+		parquet.WithCompression(compress.Codecs.Snappy),
+		parquet.WithDataPageVersion(parquet.DataPageV1),
+		parquet.WithVersion(parquet.V1_0),
+		parquet.WithDictionaryDefault(true),
+	)
+
+	pqWriter, err := pqarrow.NewFileWriter(table.Schema(), &buf, writerProps, pqarrow.DefaultWriterProps())
+	if err != nil {
+		log.Errorf("Failed to create Parquet writer: %v", err)
+		return
+	}
+
+	if err := pqWriter.WriteTable(table, table.NumRows()); err != nil {
+		log.Errorf("Failed to write record to Parquet: %v", err)
+		return
+	}
+	if err := pqWriter.Close(); err != nil {
+		log.Errorf("Failed to close Parquet writer: %v", err)
+		return
+	}
+
+	finalData := buf.Bytes()
+	dataLen := int64(buf.Len())
+
+	// generate partions
+	partitionPath := createPathPartition(config)
+
+	info, err := minioClient.PutObject(ctx, bucketName, partitionPath, bytes.NewReader(finalData), dataLen, minio.PutObjectOptions{
+		ContentType: "application/octet-stream",
+	})
+	if err != nil {
+		log.Errorf("Failed to upload Parquet file to MinIO: %v", err)
+		return
+	}
+
+	log.Infof("Successfully uploaded Parquet file to MinIO: %s (size: %d bytes)", info.Key, info.Size)
+}
+
+func readFileRemote(ctx context.Context, minioClient *minio.Client, objectName, format string, pool memory.Allocator) (arrow.Table, error) {
+	var table arrow.Table
 	object, err := minioClient.GetObject(ctx, bucketName, objectName, minio.GetObjectOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get object from MinIO: %w", err)
 	}
 	defer object.Close()
 
-	reader := csv.NewInferringReader(
-		object,
-		csv.WithAllocator(pool),
-		csv.WithChunk(10000),
-		csv.WithHeader(true),
-		csv.WithComma(','),
-		csv.WithComment('#'),
-		csv.WithNullReader(true, "NULL", "null", ""),
-	)
-	defer reader.Release()
+	if format == "csv" {
+		reader := csv.NewInferringReader(
+			object,
+			csv.WithAllocator(pool),
+			csv.WithChunk(10000),
+			csv.WithHeader(true),
+			csv.WithComma(','),
+			csv.WithComment('#'),
+			csv.WithNullReader(true, "NULL", "null", ""),
+		)
+		defer reader.Release()
 
-	var records []arrow.Record
-	for reader.Next() {
-		rec := reader.Record()
-		rec.Retain()
-		records = append(records, rec)
-	}
+		var records []arrow.Record
+		for reader.Next() {
+			rec := reader.Record()
+			rec.Retain()
+			records = append(records, rec)
+		}
 
-	if reader.Err() != nil {
-		return nil, fmt.Errorf("error reading CSV file: %w", reader.Err())
-	}
+		if reader.Err() != nil {
+			return nil, fmt.Errorf("error reading CSV file: %w", reader.Err())
+		}
 
-	if len(records) == 0 {
-		return nil, fmt.Errorf("no records found in CSV file")
-	}
+		if len(records) == 0 {
+			return nil, fmt.Errorf("no records found in CSV file")
+		}
 
-	table := array.NewTableFromRecords(reader.Schema(), records)
+		table = array.NewTableFromRecords(reader.Schema(), records)
 
-	// free records
-	for _, rec := range records {
-		rec.Release()
+		// free records
+		for _, rec := range records {
+			rec.Release()
+		}
+	} else if format == "parquet" {
+		parquetReader, err := file.NewParquetReader(object)
+		if err != nil {
+			return nil, fmt.Errorf("Reader parquet err %v", err)
+		}
+		table = array.NewTableFromRecords(parquetReader.MetaData().Schema, parquetReader)
+
 	}
 
 	return table, nil
+}
+
+func createPathPartition(config TablesConfig) string {
+	var builder strings.Builder
+	// reservation memory string
+	builder.Grow(len(config.Dest) + len(config.TableName) + (len(config.Partitions) * 30) + 45)
+
+	builder.WriteString(config.Dest)
+	builder.WriteByte('/')
+	builder.WriteString(config.TableName)
+	builder.WriteByte('/')
+
+	for _, part := range config.Partitions {
+		builder.WriteString(part.PartitionKey)
+		builder.WriteByte('=')
+		builder.WriteString(part.PartitionValue)
+		builder.WriteByte('/')
+	}
+	builder.WriteString("part-")
+	builder.WriteString(uuid.New().String())
+	builder.WriteString(".parquet")
+	return builder.String()
 }
