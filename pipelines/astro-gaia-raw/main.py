43c2d190728c8616405b530e8d87c4ad6d167f06
karchx <kenethra.74@gmail.com>
2026-06-18 14:05:12 -0600

    feat: straw raw data astroquery

--- pipelines/astro-gaia-raw/main.py
+++ pipelines/astro-gaia-raw/main.py
@@ -1,12 +1,14 @@
 import os
-import time
 import logging
+import sys
 import boto3
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from astroquery.gaia import Gaia
+from astroquery.simbad import Simbad
+from astropy.coordinates import SkyCoord, search_around_sky
+from astropy.table import Table
+import astropy.units as u
 import polars as pl
-import pyarrow as pa
-import pyarrow.parquet as pq
+import numpy as np
 
 # Basic configuration
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,126 +18,114 @@ AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 BUCKET_NAME = os.environ.get('BUCKET_NAME', 'gaia-source')
 
 S3_PREFIX="raw"
-s3_client = boto3.client(
-    's3', 
-    endpoint_url=S3_ENDPOINT, 
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-)
-MAX_WORKERS = 4  # Gaia restricts concurrent connections, 4 is a safe limit.
-CHUNK_SIZE_CSV = 100000  # Rows loaded into RAM simultaneously per worker
-TOTAL_RECORDS = 89_946_381
-PARTITIONS = 10 # Split the 89M into 10 async jobs of ~8.9M each
 
-def execute_and_download_chunk(partition_id, start_idx, end_idx):
-    """
-    Launches an async job, downloads it to disk, and incrementally converts it to Parquet.
-    """
-    temp_csv = f"temp_gaia_part_{partition_id}.csv"
-    temp_parquet = f"temp_gaia_part_{partition_id}.parquet"
-    s3_key = f"{S3_PREFIX}/part_{partition_id:04d}.parquet"
-    
-    # Your base ADQL query, injecting the random_index limits
-    # Adjust columns according to your needs to reduce I/O.
-    query = f"""
-    SELECT * FROM gaiadr3.gaia_source
-    WHERE random_index >= {start_idx} AND random_index < {end_idx}
-    AND parallax_over_error > 10 AND ruwe < 1.2
-    """
-    
-    start_time = time.time()
-    
-    # Retry management
-    max_retries = 3
-    for attempt in range(max_retries):
-        try:
-            logging.info(f"[Part {partition_id}] Starting Async Job (Attempt {attempt+1})")
-            
-            # 1. Launch async job and download directly to disk
-            job = Gaia.launch_job_async(
-                query=query,
-                dump_to_file=True,
-                output_file=temp_csv,
-                output_format="csv" # CSV is crucial for easy iteration in pandas
-            )
-            
-            logging.info(f"[Part {partition_id}] Download completed. Converting to Parquet...")
-            
-            # 2. Read CSV in streaming mode and write to Parquet
-            writer = None
-            total_rows = 0
-            
-            # Iterate over the downloaded CSV without loading it entirely into RAM
-            for chunk in pl.scan_csv(temp_csv).collect_batches(chunk_size=CHUNK_SIZE_CSV):
-                table = pa.table(chunk)
-                
-                # Initialize the Parquet writer with the schema from the first chunk
-                if writer is None:
-                    writer = pq.ParquetWriter(temp_parquet, table.schema, compression='snappy')
-                
-                writer.write_table(table)
-                total_rows += len(chunk)
-            
-            if writer:
-                writer.close()
+def get_s3_client():
+   return boto3.client(
+        's3', 
+        endpoint_url=S3_ENDPOINT, 
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
 
-            logging.info(f"upload {partition_id} to minio")
-            s3_client.upload_file(temp_parquet, BUCKET_NAME, s3_key)
-            
-            if os.path.exists(temp_csv): os.remove(temp_csv)
-            if os.path.exists(temp_parquet): os.remove(temp_parquet)
-                
-            elapsed = time.time() - start_time
-            throughput = total_rows / elapsed if elapsed > 0 else 0
-            
-            logging.info(f"[Part {partition_id}] SUCCESS: {total_rows} rows written in {elapsed:.2f}s "
-                         f"({throughput:.0f} rows/s).")
-            return total_rows
-            
-        except Exception as e:
-            logging.error(f"[Part {partition_id}] Error: {str(e)}")
-            if attempt == max_retries - 1:
-                logging.error(f"[Part {partition_id}] Permanent failure after {max_retries} attempts.")
-                # Ensure temp file cleanup in case of critical error
-                if os.path.exists(temp_csv): os.remove(temp_csv)
-                raise
-            time.sleep(2 ** attempt) # Exponential backoff fallback
+def extract_aries_data():
+    temp_votable = "aries_temp.vot.gz"
+    temp_parquet = "aries_star_cluster.parquet"
+    s3_key = f"{S3_PREFIX}/{temp_parquet}"
+    logging.warning(f"S3 path {s3_key}")
+    try:
+        coord = SkyCoord('02h07m10.40s', '+23d27m44.7s', frame='icrs')
+        table_simbad = Simbad.query_region(coord, radius="1d")
+
+        if table_simbad is None:
+            raise ValueError("SIMBAD not data")
+
+        valid_mask = ~table_simbad['ra'].mask & ~table_simbad['dec'].mask
+        table_simbad = table_simbad[valid_mask]
+        coords_simbad = SkyCoord(table_simbad['ra'], table_simbad['dec'], unit=(u.hourangle, u.deg))
+
+        ra_center_deg = coord.ra.deg
+        dec_center_deg = coord.dec.deg
+
+        logging.info(f"ra_center: {ra_center_deg} dec_center_deg: {dec_center_deg}")
+        query_gaia = f"""
+        SELECT
+           source_id, ra, dec, parallax, pmra, pmdec, phot_g_mean_mag
+        FROM gaiadr3.gaia_source
+        WHERE 1=CONTAINS(POINT('ICRS', ra, dec), CIRCLE('ICRS', {ra_center_deg}, {dec_center_deg}, 1.0))
+        """
+        logging.info("Async and download in disk...")
+
+        max_retries = 3
+
+        for attempt in range(max_retries):
+            try:
+                job = Gaia.launch_job(query=query_gaia)
+                table_gaia = job.get_results()
+                table_gaia.write(temp_votable, format="votable", overwrite=True)
+                break
+            except Exception as e:
+                if attempt == max_retries - 1:
+                    logging.error("Exceded retries")
+                    raise e
+
+                logging.error(f"retry {attempt}...")
+                logging.error("Error", exc_info=True)
+
+
+        logging.info("Process VOTable...")
+
+        table_gaia = Table.read(temp_votable)
+        if len(table_gaia) == 0:
+            logging.warning("Empty VOTable")
+            return
+
+        coords_gaia = SkyCoord(table_gaia['ra'], table_gaia['dec'], unit=(u.deg, u.deg))
+
+        # Cross match
+        logging.info("Cross-match init...")
+
+        max_sep = 0.25 * u.deg
+        idx_simbad, idx_gaia, d2d, d3d = search_around_sky(coords_simbad, coords_gaia, max_sep)
+        logging.info(f"Cross-match. {len(idx_simbad)} find clusters.")
+
+        matched_simbad = table_simbad[idx_simbad]
+        matched_gaia = table_gaia[idx_gaia]
+
+        df_simbad = pl.DataFrame({
+            "source_cluster": np.array(matched_simbad["main_id"]).astype(str)
+        })
+
+        df_gaia = pl.DataFrame({
+            "source_id": np.array(matched_gaia['source_id']),
+            "ra": np.array(matched_gaia['ra']),
+            "dec": np.array(matched_gaia['dec']),
+            "parallax": np.array(matched_gaia['parallax']),
+            "pmra": np.array(matched_gaia['pmra']),
+            "pmdec": np.array(matched_gaia['pmdec']),
+            "phot_g_mean_mag": np.array(matched_gaia['phot_g_mean_mag'])
+        })
+
+        df_all = pl.concat([df_gaia, df_simbad], how="horizontal")
+
+        df_all.write_parquet(temp_parquet, compression='snappy')
+
+        logging.info(f"upload to minio")
+        s3_client = get_s3_client()
+        s3_client.upload_file(temp_parquet, BUCKET_NAME, s3_key)
+        logging.info("EXTRACTION COMPLETED")
+    except Exception as e:
+        logging.error(f"Error in load", exc_info=True)
+        sys.exit(1)
+    finally:
+        for f in [temp_votable, temp_parquet]:
+            if os.path.exists(f):
+                os.remove(f)
 
 def main():
-    # Assuming we want to filter a subset, map the random_index range
-    # For 89M records (approx 5% of the 1.8 billion Gaia catalog)
-    # Adjust the indices according to your actual WHERE filter.
-    index_step = TOTAL_RECORDS // PARTITIONS
-    tasks = []
-    
-    # Generate boundaries for each partition
-    for i in range(PARTITIONS):
-        start = i * index_step
-        end = start + index_step if i < PARTITIONS - 1 else TOTAL_RECORDS
-        tasks.append((i, start, end))
-        
-    total_processed = 0
-    global_start = time.time()
-    
-    # Execute in parallel (I/O bound)
-    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        # Map futures to their IDs for better traceability
-        future_to_part = {executor.submit(execute_and_download_chunk, t[0], t[1], t[2]): t[0] for t in tasks}
-        
-        for future in as_completed(future_to_part):
-            part_id = future_to_part[future]
-            try:
-                rows = future.result()
-                total_processed += rows
-            except Exception as exc:
-                logging.error(f"Partition {part_id} generated an exception: {exc}")
-
-    global_elapsed = time.time() - global_start
     logging.info("="*50)
-    logging.info("EXTRACTION COMPLETED")
-    logging.info(f"Total records: {total_processed}")
-    logging.info(f"Total time: {global_elapsed / 60:.2f} minutes")
-    logging.info(f"Global throughput: {total_processed / global_elapsed:.0f} rows/second")
+    extract_aries_data()
+    logging.info("="*50)
 
 if __name__ == "__main__":
     main()
+
