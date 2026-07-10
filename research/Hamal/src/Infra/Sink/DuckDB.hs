8539d1b45bc5ad156b6f66092e8c5dc49f662b99
karchx <kenethra.74@gmail.com>
2026-07-09 18:22:29 -0600

    feat: add connection duckdb

--- research/Hamal/src/Infra/Sink/DuckDB.hs
+++ research/Hamal/src/Infra/Sink/DuckDB.hs
@@ -1,1 +1,12 @@
-module Infra.Sink.DuckDB where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Infra.Sink.DuckDB (fetchFactAries) where
+
+import Data.Text (Text)
+import Database.DuckDB.Simple
+
+fetchFactAries :: Connection -> IO [Maybe Text]
+fetchFactAries conn = do
+    fmap fromOnly <$> query_ conn "SELECT source_id FROM haumea.gold.fact_aries LIMIT 10"
+
+
--- research/Hamal/src/Main.hs
+++ research/Hamal/src/Main.hs
@@ -1,5 +1,54 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
+
+import Data.Text
+import Database.DuckDB.Simple
+import System.Environment (lookupEnv)
+import System.Exit (die)
+import Infra.Sink.DuckDB
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  maybeKey <- lookupEnv "AWS_ACCESS_KEY_ID"
+  maybeSecret <- lookupEnv "AWS_SECRET_ACCESS_KEY"
+  maybeEndpoint <- lookupEnv "AWS_ENDPOINT"
+
+  (keyId, secretKey, endpoint) <- case (maybeKey, maybeSecret, maybeEndpoint) of
+    (Just k, Just s, Just e) -> return (k, s, e)
+    _ -> die "Error fatal: Keys storage not found"
+
+  withConnection ":memory:" $ \conn -> do
+        _ <- execute_ conn "INSTALL iceberg; LOAD iceberg;"
+        _ <- execute_ conn "INSTALL httpfs; LOAD httpfs;"
+
+        let secretQuery = mconcat 
+              [ "CREATE SECRET (TYPE S3, "
+              , "KEY_ID '", keyId, "', "
+              , "SECRET '", secretKey, "', "
+              , "ENDPOINT '", endpoint, "', "
+              , "URL_STYLE 'path', "
+              , "USE_SSL 'false');"
+              ]
+
+        _ <- execute_ conn (Query (pack secretQuery))
+
+        let icebergSecretQuery = mconcat
+              [ "CREATE SECRET lk_secret ("
+              , "TYPE ICEBERG, "
+              , "TOKEN 'dummy');"
+              ]
+        _ <- execute_ conn (Query (pack icebergSecretQuery))
+
+        let attachQuery = mconcat
+                [ "ATTACH 'datalake-haumea' AS haumea ("
+                , "TYPE iceberg, "
+                , "ENDPOINT 'http://localhost:8181/catalog', "
+                , "SECRET 'lk_secret'"
+                , ");"
+                ]
+        _ <- execute_ conn (Query (pack attachQuery))
+        putStrLn "Iceberg connection done..."
+        tables <- fetchFactAries conn
+        print tables
+
